f7172fa5382ea6d0d217993768608d1af5497860
Miles Noble <105387328+coding-noble@users.noreply.github.com>
2024-12-11 10:30:57 -0700

    Update Sagger Host

--- utils/swagger.js
+++ utils/swagger.js
@@ -5,8 +5,8 @@ const doc = {
         title: "The Noble Cookbook API",
         description: "Swagger API documentation for the Noble Cookbook web service."
     },
-    host: process.env.NODE_ENV === 'production' ? 'your-production-host.com' : 'localhost:2600',
-    schemes: [process.env.NODE_ENV === 'production' ? 'https' : 'http']
+    host: process.env.HOST || 'localhost:2600',
+    schemes: ['http', 'https']
 };
 
 const outputFile = "./swagger.json";
