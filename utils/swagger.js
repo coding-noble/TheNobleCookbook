f0c16d577aec3c5db8bb4d5fcfba3d889f92de07
Miles Noble <105387328+coding-noble@users.noreply.github.com>
2024-12-11 12:11:54 -0700

    Render Compatibility

--- utils/swagger.js
+++ utils/swagger.js
@@ -5,8 +5,8 @@ const doc = {
         title: "The Noble Cookbook API",
         description: "Swagger API documentation for the Noble Cookbook web service."
     },
-    host: "localhost:2600",
-    schemes: ['http', 'https']
+    host: "https://the-noble-cookbook.onrender.com",
+    schemes: ['https']
 };
 
 const outputFile = "./swagger.json";
